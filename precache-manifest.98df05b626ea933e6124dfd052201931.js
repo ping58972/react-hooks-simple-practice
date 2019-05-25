488b1ebb2e9e04eac5d1391785c9c81e64dbf489
ping58972 <ping58972@gmail.com>
2019-05-25 17:20:51 -0500

    Updates

---
+++ precache-manifest.98df05b626ea933e6124dfd052201931.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e84bcb21f5cc5e802666df90c5046fc4",
+    "url": "/react-hooks-simple-practice/index.html"
+  },
+  {
+    "revision": "d0ccc405a22128386531",
+    "url": "/react-hooks-simple-practice/static/js/2.40044159.chunk.js"
+  },
+  {
+    "revision": "d2ac10bed2a9a33ab207",
+    "url": "/react-hooks-simple-practice/static/js/main.dff723b2.chunk.js"
+  },
+  {
+    "revision": "fb4aa3ace41544497046",
+    "url": "/react-hooks-simple-practice/static/js/runtime~main.33627e3a.js"
+  }
+]);
